90bb1429f6c9cc98ad2af5e66b5bdc00a4792d7c
liujunjie <livjunjie@gmail.com>
2021-05-01 05:12:13 +0800

    update test photos
    update

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,9 +116,10 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/LutteMars/blog-configuration/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/LutteMars/blog-configuration/master/photos/' + data.link[i];
-          // var src = 'https://raw.githubusercontent.com/LutteMars/blog-configuration/master/photos/' + data.link[i];
+          // var minSrc = 'https://github.com/LutteMars/blog-configuration/master/min_photos/' + data.link[i];
+          // var src = 'https://github.com/LutteMars/blog-configuration/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/LutteMars/blog-configuration/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/LutteMars/blog-configuration/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '');
           src += '';
